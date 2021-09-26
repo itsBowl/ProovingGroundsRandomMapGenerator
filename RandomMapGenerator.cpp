f6cab140215dc2736382249051c778405c93051b
itsBowl <82961540+itsBowl@users.noreply.github.com>
2021-09-26 02:28:18 +0100

    version 2

--- RandomMapGenerator.cpp
+++ RandomMapGenerator.cpp
@@ -4,30 +4,20 @@
 #include <vector>
 #include <string>
 #include <random>
-
-int getRandomNumber(int min, int max) {
-	double fraction{ 1.0 / (RAND_MAX + 1.0) };
-	return min + static_cast<int>((max - min + 1) * (std::rand() * fraction));
-}
-
+#include <cstdio>
 
 
 int main() {
-	int mapNumber{};
 	std::vector<std::string> maps = { "Altar of Flame", "Anomaly", "Bannerfall", "Burnout", "Cauldron", "Convergence", "Dead Cliffs", "Endless Vale", "Exodus Blue", "Fortress", "Fragment", "Javelin-4", "Midtown", "Pacifica", "Radiant Cliffs", "rusted Lands", "Twilight Gap", "Widow's Court", "Wormhaven" };
 	std::mt19937 mersenne{ static_cast<std::mt19937::result_type>(std::time(nullptr)) };
 	std::uniform_int_distribution<> die{ 1, 20 };
-	//std::cout << size(maps) << '\n';
+	int map_length = maps.size();
 	die(mersenne);
 	std::cout << "First Number Discarded (Ensures better random generation)\n";
-	mapNumber = die(mersenne);
-	std::cout << "First Map: " << maps[mapNumber - 1] << " Map Number: " << mapNumber << '\n';
-	std::system("pause");
-	mapNumber = die(mersenne);
-	std::cout << "Second Map: " << maps[mapNumber - 1] << " Map Number: " << mapNumber << '\n';
-	std::system("pause");
-	mapNumber = die(mersenne);
-	std::cout << "Third Map: " << maps[mapNumber - 1] << " Map Number: " << mapNumber << '\n';
+	int map_1 = die(mersenne) - 1;
+	int map_2 = die(mersenne) - 1;
+	int map_3 = die(mersenne) - 1;
+	std::cout << "Maps are, in order: " << maps[map_1] << ", " << maps[map_2] << ", " << maps[map_3] << '\n';
 	std::system("pause");
 	return 0;
 }
